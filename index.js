0da16eae5b734d720812962486af33cbc5a6839e
jurusteve <jstevemu17@gmail.com>
2019-04-24 15:32:49 -0400

    set up project deps

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code away!
+const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
+
+const server = express()
+server.use(express.json())
+server.use(helmet())
+server.use(morgan('dev'))
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h1>Welcome To Lambda Blog API</h1>
+    `)
+})
+server.use('/api/posts', postsRouter)
+server.use('/api/users', usersRouter)
+
+server.listen(4003, () => {
+  console.log('Api Ready')
+})
